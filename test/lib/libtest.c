c4911ca6d0c92eb33c90181e1cdb97df5f135a9a
nots1dd <sid9.karanam@gmail.com>
2024-09-24 19:55:20 +0530

    [CHORE] testlib conf

--- test/lib/libtest.c
+++ test/lib/libtest.c
@@ -2,20 +2,29 @@
 
 /****************************************
  * libtest.c
- * 
- * This file contains the implementation of the dynamic 
- * library functions. They will be hot-reloaded during 
- * runtime by the main program.
+ *
+ * This is the implementation of the dynamic library. The functions
+ * defined here will be dynamically loaded at runtime by the main
+ * program (main.c). These functions can be updated and reloaded 
+ * during runtime, enabling "hot reloading."
+ *
+ * Functions:
+ * - easC_init: Initializes the library and prints a message.
+ * - easC_print: Prints a message for testing purposes.
+ * - easC_update: Updates logic and demonstrates hot reloading.
  ****************************************/
 
+/* Initializes the library */
 void easC_init() {
     printf("Library initialized successfully.\n");
 }
 
+/* Prints a simple message for testing */
 void easC_print() {
     printf("This is the test_print function from the dynamically loaded library.\n");
 }
 
+/* Updates the logic and prints a message */
 void easC_update() {
-    printf("Updated function call. Hot reloading works!\n");
+    printf("Hemlo world?\n??\n%x%x%x\n");
 }
